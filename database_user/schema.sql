6ffb5e53f5839b8614d99a877b5f693ff1c8cf7e
sabrinacamurati <sabrina.camurati@gmail.com>
2017-05-10 01:03:39 +0200

    small bug fix

--- database_user/schema.sql
+++ database_user/schema.sql
@@ -1,20 +1,28 @@
+CREATE TYPE yesNo AS ENUM('Y', 'N');
+CREATE TYPE masFem AS ENUM('M', 'F');
+CREATE TYPE istruzione AS ENUM('LICENZA ELEMENTARE', 'LICENZA MEDIA', 'DIPLOMA DI ISTRUZIONE SECONDARIA SUPERIORE', 'LAUREA DI PRIMO LIVELLO', 'LAUREA SPECIALISTICA', 'MASTER UNIVERSITARIO DI PRIMO LIVELLO', 'MASTER UNIVERSITARIO DI SECONDO LIVELLO');
+CREATE TYPE occ AS ENUM('STUDENTE', 'DISOCCUPATO', 'OCCUPATO AUTONOMO', 'OCCUPATO PERMANENTE', 'OCCUPATO TEMPORANEO', 'PENSIONATO');
+CREATE TYPE carb AS ENUM ('BENZINA', 'DIESEL', 'GPL', 'GASOLIO', 'METANO', 'AUTO ELETTRICA');
+CREATE TYPE forn AS ENUM('ENJOY', 'CAR2GO', 'BLUETORINO');
+CREATE TYPE freq AS ENUM('BUS', 'TRAM', 'METRO', 'TAXI');
+CREATE TYPE stato AS ENUM('AUTENTICATO', 'NON_AUTENTICATO', 'BANNATO');
 create table if not exists UsersTable (
   username varchar(30) not null,
   email varchar(200) not null,
   password varchar(200) not null,
-  genere ENUM('M', 'F'), 
+  genere masFem, 
   data_di_nascita  DATE, 
-  livello_istruzione ENUM('LICENZA ELEMENTARE', 'LICENZA MEDIA', 'DIPLOMA DI ISTRUZIONE SECONDARIA SUPERIORE', 'LAUREA DI PRIMO LIVELLO', 'LAUREA SPECIALISTICA', 'MASTER UNIVERSITARIO DI PRIMO LIVELLO', 'MASTER UNIVERSITARIO DI SECONDO LIVELLO'), 
-  occupazione ENUM('STUDENTE', 'DISOCCUPATO', 'OCCUPATO AUTONOMO', 'OCCUPATO PERMANENTE', 'OCCUPATO TEMPORANEO'), 
-  ha_auto_privata ENUM('Y', 'N'), 
+  livello_istruzione istruzione, 
+  occupazione occ, 
+  ha_auto_privata yesNo, 
   auto_anno_immatricolazione Int, 
-  auto_tipo_carburante ENUM ('BENZINA', 'DIESEL', 'GPL', 'GASOLIO', 'METANO', 'AUTO ELETTRICA'), 
-  usa_car_sharing ENUM('Y', 'N'), 
-  car_sharing_fornitore ENUM('ENJOY', 'CAR2GO', 'BLUETORINO'), 
-  usa_bike_sharing ENUM('Y', 'N'), 
-  ha_bici_privata ENUM('Y', 'N'),
-  usa_mezzi_pubblici ENUM('Y', 'N'),
-  titolo_viaggio_frequente ENUM('BUS', 'TRAM', 'METRO', 'TAXI'), 
-  stato_utente ENUM('AUTENTICATO', 'NON_AUTENTICATO', 'BANNATO'),
+  auto_tipo_carburante carb, 
+  usa_car_sharing yesNo, 
+  car_sharing_fornitore forn, 
+  usa_bike_sharing yesNo, 
+  ha_bici_privata yesNo,
+  usa_mezzi_pubblici yesNo,
+  titolo_viaggio_frequente freq, 
+  stato_utente stato,
   primary key (email)
 );
