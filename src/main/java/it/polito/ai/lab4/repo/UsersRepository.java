b705b9b377c9c6e835f4022e05f56c6ab2635937
Martino Mensio <martinomensio@outlook.it>
2017-05-11 18:56:49 +0200

    security and authentication configuration

--- src/main/java/it/polito/ai/lab4/repo/UsersRepository.java
+++ src/main/java/it/polito/ai/lab4/repo/UsersRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.repository.CrudRepository;
 import it.polito.ai.lab4.repo.entities.User;
 
 public interface UsersRepository extends CrudRepository<User, Long> {
-
+	User findByEmail(String email);
 }
