bf5da52cd1bdbab4657d17482ac555ae6c8f4cc9
SabryKarate <sabrina.camurati@gmail.com>
2017-05-13 03:09:56 +0200

    updated create.sh file
    I included both users and messages

--- database/create.sh
+++ database/create.sh
@@ -6,6 +6,9 @@ export PGPASSWORD=ai-user-password
 
 dropdb -h localhost -U postgres $DB_NAME
 createdb -h localhost -U postgres $DB_NAME
-psql -h localhost -U postgres -d $DB_NAME -f schema.sql
-psql -h localhost -U postgres -d $DB_NAME -f tabelle_valori.sql
-psql -h localhost -U postgres -d $DB_NAME -f utenti_esempio.sql
+psql -h localhost -U postgres -d $DB_NAME -f schema_users.sql
+psql -h localhost -U postgres -d $DB_NAME -f values_tables.sql
+psql -h localhost -U postgres -d $DB_NAME -f users_example.sql
+psql -h localhost -U postgres -d $DB_NAME -f schema_messages.sql
+psql -h localhost -U postgres -d $DB_NAME -f values_topics.sql
+psql -h localhost -U postgres -d $DB_NAME -f messages_example.sql
