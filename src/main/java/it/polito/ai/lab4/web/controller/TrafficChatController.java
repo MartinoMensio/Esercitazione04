b541349ac652168d21a3ac6e7501e35fec7b9614
Riccardo Persiani <s225289@studenti.polito.it>
2017-05-13 19:37:13 +0200

    Chat window added

--- src/main/java/it/polito/ai/lab4/web/controller/TrafficChatController.java
+++ src/main/java/it/polito/ai/lab4/web/controller/TrafficChatController.java
@@ -6,12 +6,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@RequestMapping({"/registration"})
-public class RegistrationController {
+@RequestMapping({"/traffic"})
+public class TrafficChatController {
 	
 	@RequestMapping(method = RequestMethod.GET)
 	public String showLogin(ModelMap model){
-		return "registration";
+		return "traffic";
 	}
-
 }
