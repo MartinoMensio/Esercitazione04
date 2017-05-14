452c000982e2bb9fb7f5339fc4da30a7bc13a3e1
Riccardo Persiani <s225289@studenti.polito.it>
2017-05-14 17:04:37 +0200

    Profile scheleton

--- database/users_example.sql
+++ database/users_example.sql
@@ -5,7 +5,7 @@ INSERT INTO users(nickname, email, password, status_id) values ('bea2005', 'beau
 INSERT INTO users(nickname, email, password, status_id) values ('UtenteNonAutenticato', 'nonautenticato@hotmail.com', crypt('12345678', gen_salt('bf')), 2);
 INSERT INTO users(nickname, email, password) values ('martino', 'martino@polito.it', crypt('martino', gen_salt('bf')));
 INSERT INTO users(nickname, email, password) values ('alessio', 'alessio@polito.it', crypt('alessio', gen_salt('bf')));
-INSERT INTO users(nickname, email, password) values ('riccardo', 'riccardo@polito.it', crypt('ricccardo', gen_salt('bf')));
+INSERT INTO users(nickname, email, password) values ('riccardo', 'riccardo@polito.it', crypt('riccardo', gen_salt('bf')));
 INSERT INTO users(nickname, email, password) values ('sabrina', 'sabrina@polito.it', crypt('sabrina', gen_salt('bf')));
 INSERT INTO user_profiles (id, sex, date_of_birth, education_level_id, employment_id, private_car_ownership, car_registration_year, fuel_id, car_sharing_usage, bike_sharing_usage, private_bike_usage, public_transport_usage)
 values (1, 'M', '1967-04-26', 5, 3, 'Y', 2005, 2, 'N', 'N', 'Y', 'N');
