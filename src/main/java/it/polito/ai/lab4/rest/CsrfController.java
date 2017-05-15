07b2b93d2a71951b4e93303780b915232adfaaa5
SabryKarate <sabrina.camurati@gmail.com>
2017-05-15 14:25:55 +0200

    draft controllers
    TODO UserController

--- src/main/java/it/polito/ai/lab4/rest/CsrfController.java
+++ src/main/java/it/polito/ai/lab4/rest/CsrfController.java
@@ -1,21 +1,12 @@
 package it.polito.ai.lab4.rest;
 
-import java.util.Set;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import it.polito.ai.lab4.business.services.chat.ChatService;
-import it.polito.ai.lab4.repo.entities.Topic;
 
 @RestController
 public class CsrfController {
-
-	@Autowired
-	private ChatService chatService;
-	
 	
 	/**
 	 * Returns the csrf token for connecting to the secured WebSocket
@@ -25,10 +16,5 @@ public class CsrfController {
     @RequestMapping("/rest/csrf")
     public CsrfToken csrf(CsrfToken token) {
         return token;
-    }
-    
-    @RequestMapping("/rest/topics")
-    public Set<Topic> topicsReturn() {
-        return chatService.getTopics();
     }
 }
---
+++ src/main/java/it/polito/ai/lab4/rest/UsersController.java
@@ -0,0 +1,11 @@
+package it.polito.ai.lab4.rest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class UsersController {
+	
+	// TODO
+}
