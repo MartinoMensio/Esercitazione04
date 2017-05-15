3a8d9f87c64969ca125572c26fbc7ad7bef1c7a3
aleciav <ale.ciav92@gmail.com>
2017-05-15 17:14:41 +0200

    Preparation fo visualization of user's profile data
    Has to be completed with thymeleaf tags

--- src/main/java/it/polito/ai/lab4/web/controller/ProfileController.java
+++ src/main/java/it/polito/ai/lab4/web/controller/ProfileController.java
@@ -5,6 +5,8 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import it.polito.ai.lab4.repo.entities.User;
+
 @Controller
 @RequestMapping({"/profile"})
 public class ProfileController extends AbstractPageWithHeaderController {
@@ -12,6 +14,10 @@ public class ProfileController extends AbstractPageWithHeaderController {
 	@RequestMapping(method = RequestMethod.GET)
 	public String showLogin(ModelMap model){
 		super.attachData(model);
+		User currentUser = currentUserService.getCurrentUser();
+		
+		model.addAttribute("profileInfo", currentUser.getProfile());
+		
 		return "profile";
 	}
 
