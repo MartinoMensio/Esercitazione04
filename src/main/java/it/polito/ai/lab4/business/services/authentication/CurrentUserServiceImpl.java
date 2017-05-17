76d5e26cc6f1d3a8f60efafd60b372ed0fbccb70
Martino Mensio <martinomensio@outlook.it>
2017-05-17 17:32:42 +0200

    fix authenticated check

--- src/main/java/it/polito/ai/lab4/business/services/authentication/CurrentUserServiceImpl.java
+++ src/main/java/it/polito/ai/lab4/business/services/authentication/CurrentUserServiceImpl.java
@@ -1,6 +1,7 @@
 package it.polito.ai.lab4.business.services.authentication;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
@@ -17,7 +18,7 @@ public class CurrentUserServiceImpl implements CurrentUserService {
 	@Override
 	public User getCurrentUser() {
 		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-		if (auth != null) {
+		if (!(auth instanceof AnonymousAuthenticationToken)) {
 			String email = auth.getName();
 			return userRepository.findByEmail(email);
 		} else {
