8bdc6c4a2187eaf45931b691a7bb884ed0536df7
Martino Mensio <martinomensio@outlook.it>
2017-05-18 23:49:44 +0200

    message details added

--- src/main/java/it/polito/ai/lab4/rest/resources/ChatMessageResource.java
+++ src/main/java/it/polito/ai/lab4/rest/resources/ChatMessageResource.java
@@ -1,15 +1,22 @@
 package it.polito.ai.lab4.rest.resources;
 
+import java.util.Calendar;
+
+import org.springframework.hateoas.Link;
 import org.springframework.hateoas.ResourceSupport;
 
 import it.polito.ai.lab4.business.services.chat.ChatMessage;
 
 public class ChatMessageResource extends ResourceSupport{
-	
+
 	public String text;
+	public Calendar sendingTime;
+	public String image;
 	
 	public ChatMessageResource(ChatMessage chatMessage) {
 		this.text = chatMessage.getText();
+		this.sendingTime = chatMessage.getSendingTime();
+		this.image = chatMessage.getImageUrl();
 	}
 
 }
